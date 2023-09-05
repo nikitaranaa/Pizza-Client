85f20bb0d89173911bba19d0fd20cd9e1f863989
Nikita Rana <91747922+nikitaranaa@users.noreply.github.com>
2023-09-05 10:41:47 +0530

    Update store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
 import CartSlice from "./slices/CartSlice";
 import UserSlice from "./slices/UserSlice";
+import AdminSlice from './slices/AdminSlice'
+import MenuSlice from "./slices/MenuSlice";
 import { combineReducers } from "redux";
 import { createCookieMiddleware, saveStateToCookie, loadStateFromCookie } from "../middlewares/cookieMiddleware";
 
 // Combine your reducers
 const rootReducer = combineReducers({
   cart: CartSlice,
-  user: UserSlice
+  user: UserSlice,
+  admin : AdminSlice,
+  menu : MenuSlice
 });
 
 // Load the initial state from the cookie
