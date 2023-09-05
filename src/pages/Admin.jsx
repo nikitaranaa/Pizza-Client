be3617a183da187aa59f110cca7300d587d14065
Nikita Rana <91747922+nikitaranaa@users.noreply.github.com>
2023-09-05 10:35:06 +0530

    Update Admin

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,16 +1,19 @@
 import React, { useEffect, useState } from 'react'
 import OrderRow from '../components/OrderRow'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
+import {update} from '../redux/slices/AdminSlice'
 import { io } from 'socket.io-client';
 import { toast } from 'react-hot-toast';
 const Admin = () => {
     const token = useSelector((state) => state.user.token)
     const [orders, setOrders] = useState([])
-    const socket = io('https://pizza-mania-zqoq.onrender.com')
+    const [payload, setPayload] = useState(0)
+    const socket = io('https://pizza-mania-23rd.onrender.com')
+    const dispatch = useDispatch()
     const fetchOrders = async () => {
         try {
             const response = await fetch(
-                'https://pizza-mania-zqoq.onrender.com/api/v1/admin/orders',
+                'https://pizza-mania-23rd.onrender.com/api/v1/admin/orders',
                 {
                     method: 'GET',
                     headers: {
@@ -20,19 +23,20 @@ const Admin = () => {
             );
             const data = await response.json();
             setOrders(data.data); // Update the orders state with the fetched data
+            setPayload(data.data.length)
+            dispatch(update(data.data.length))
         } catch (error) {
             toast.error(error)
         }
     };
     useEffect(() => {
         fetchOrders(); // Call the fetchOrders function
-    
         // Join the 'adminRoom'
         socket.emit('join', 'adminRoom');
         // Listen for the 'orderPlaced' event
         socket.on('orderPlaced', (data) => {
           toast.success('New order received')
-          setOrders((prevOrders) => [data, ...prevOrders]);
+          fetchOrders()
         });
     
         // Clean up the event listener when the component unmounts
@@ -43,15 +47,15 @@ const Admin = () => {
     return (
         <section className="orders light-section">
             <div className="container mx-auto pt-12">
-                <h1 className="font-bold text-lg mb-4">All orders</h1>
                 <table className="w-full table-auto bg-white">
                     <thead>
                         <tr>
-                            <th className="px-4 py-2 text-left">Orders</th>
-                            <th className="px-4 py-2 text-left">Customer</th>
-                            <th className="px-4 py-2 text-left">Address</th>
-                            <th className="px-4 py-2 text-left">status</th>
-                            <th className="px-4 py-2 text-left">Placed at</th>
+                            <th className="px-4 py-2 text-center">Order Details</th>
+                            <th className="px-4 py-2 text-center">Customer</th>
+                            <th className="px-4 py-2 text-center">Address</th>
+                            <th className="px-4 py-2 text-center">Cutlery Needed</th>
+                            <th className="px-4 py-2 text-center">Order Status</th>
+                            <th className="px-4 py-2 text-center">Placed at</th>
                         </tr>
                     </thead>
                     <tbody>
